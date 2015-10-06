9de02d31904199f4c63454e3498d86ae11ea222b
hanken <franco.fallica@gmail.com>
2015-10-06 22:29:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // Load the http module to create an http server.
 var http = require('http');
- 
+var os = require("os");
+
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
+  var hostname = os.hostname();
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello new World");
+  
+  response.end("Hello new World - " + hostname);
 });
  
 // Listen on port 8000, IP defaults to "0.0.0.0"
